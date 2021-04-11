6546457d44b5384353ee861f2a98dcbb790e161f
Liubomyr86 <demianchukliubomyr@gmail.com>
2021-04-11 20:53:07 +0200

    feat: add solutinos for 09 task

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,26 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let num = n;
+  let digit = 0;
+  let digit2 = 0;
+
+  while (num) {
+    digit += num % 10;
+    num = Math.floor(num / 10);
+  }
+
+  if (digit >= 10) {
+    while (digit) {
+      digit2 += digit % 10;
+      digit = Math.floor(digit / 10);
+    }
+
+    digit = digit2;
+  }
+
+  return digit;
 }
 
 module.exports = getSumOfDigits;
