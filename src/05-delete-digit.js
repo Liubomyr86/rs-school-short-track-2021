a8c7c86b9e03bb2b63a06e2c3aa9b89ed8fce29e
Liubomyr86 <demianchukliubomyr@gmail.com>
2021-04-12 12:50:57 +0200

    feat: add solutinos for 05 task

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,21 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const arr = n.toString().split('').map((elem) => +elem);
+  const min = Math.min(...arr);
+  let newNum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === min) {
+      arr.splice(i, 1);
+      break;
+    }
+  }
+
+  newNum = arr.join('');
+
+  return +newNum;
 }
 
 module.exports = deleteDigit;
